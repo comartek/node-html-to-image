38dde5d999440de3036a13fb7ca6edd44150b5cb
Truc <trucdev@gmail.com>
2022-01-17 13:39:48 +0700

    allow pass the puppeteer cluster instance

--- example.js
+++ example.js
@@ -1,7 +1,29 @@
-const nodeHtmlToImage = require('./src/index.js')
+const nodeHtmlToImage = require("./src/index.js");
+const { Cluster } = require("puppeteer-cluster");
 
-nodeHtmlToImage({
-  output: './image.png',
-  html: '<html><body>Hello world 🙌!</body></html>'
-})
-  .then(() => console.log('The image was created successfully!'))
+(async () => {
+  const cluster = await Cluster.launch({
+    concurrency: Cluster.CONCURRENCY_PAGE,
+    maxConcurrency: 10,
+    puppeteerOptions: {
+      headless: true,
+      args: ['--no-sandbox'],
+    },
+  });
+
+  cluster.on("taskerror", (err, data, willRetry) => {
+    console.log("task error", data);
+  });
+
+  for (let i = 0; i < 200; i++) {
+    try {
+      nodeHtmlToImage({
+        html: "<html><body>Hello world 🙌!</body></html>",
+        puppeteerCluster: cluster,
+      })  
+    } catch (error) {
+      console.log('ERRRRRRRRRORRRRR', error)
+    }
+    
+  }
+})();
--- src/index.js
+++ src/index.js
@@ -1,50 +1,53 @@
-const puppeteer = require('puppeteer')
-const { Cluster } = require('puppeteer-cluster')
+const puppeteer = require("puppeteer");
+const { Cluster } = require("puppeteer-cluster");
 
-const { makeScreenshot } = require('./screenshot.js')
+const { makeScreenshot } = require("./screenshot.js");
 
-module.exports = async function(options) {
+module.exports = async function (options) {
   const {
     html,
     content,
     output,
-    selector = 'body',
+    selector = "body",
     puppeteerArgs = {},
-  } = options
+    puppeteerCluster,
+  } = options;
 
   if (!html) {
-    throw Error('You must provide an html property.')
+    throw Error("You must provide an html property.");
   }
 
-  const cluster = await Cluster.launch({
-    concurrency: Cluster.CONCURRENCY_CONTEXT,
-    maxConcurrency: 2,
-    puppeteerOptions: { ...puppeteerArgs, headless: true },
-  });
+  const cluster = puppeteerCluster
+    ? puppeteerCluster
+    : await Cluster.launch({
+        concurrency: Cluster.CONCURRENCY_CONTEXT,
+        maxConcurrency: 2,
+        puppeteerOptions: { ...puppeteerArgs, headless: true },
+      });
 
-  let buffers = []
+  let buffers = [];
 
   await cluster.task(async ({ page, data: { content, output, selector } }) => {
-    const buffer = await makeScreenshot(page, { ...options, content, output, selector })    
+    const buffer = await makeScreenshot(page, {
+      ...options,
+      content,
+      output,
+      selector,
+    });
     buffers.push(buffer);
   });
 
-  cluster.on('taskerror', (err, data) => {
-    throw err
+  const shouldBatch = Array.isArray(content);
+  const contents = shouldBatch ? content : [{ ...content, output, selector }];
+
+  contents.forEach((content) => {
+    const { output, selector: contentSelector, ...pageContent } = content;
+    cluster.queue({
+      output,
+      content: pageContent,
+      selector: contentSelector ? contentSelector : selector,
+    });
   });
 
-  const shouldBatch = Array.isArray(content)
-  const contents = shouldBatch ? content : [{ ...content, output, selector }]
-
-  contents.forEach(content => {
-    const { output, selector: contentSelector, ...pageContent } = content
-    cluster.queue({ output, content: pageContent, selector: contentSelector ? contentSelector : selector })
-  })
-
-  await cluster.idle();
-  await cluster.close();
-
-  return shouldBatch ? buffers : buffers[0]
- 
-}
-
+  return shouldBatch ? buffers : buffers[0];
+};
